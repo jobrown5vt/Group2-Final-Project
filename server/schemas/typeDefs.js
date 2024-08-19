24de50c6f0ea2d15b8916ee6a52bdfc9dc79d98b
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2024-08-18 22:32:04 -0400

    mutations complete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,13 +1,9 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 // Above we require gql because we are using apollo-server-express
 
 const typeDefs = gql`
-
-    
-    
-
-    type User {
+  type User {
     _id: ID
     username: String
     email: String
@@ -15,24 +11,31 @@ const typeDefs = gql`
     highScore: [HighScore]
   }
 
-   type HighScore {
+  type HighScore {
     _id: ID
     highScoreTotal: String
     highScoreName: String
     createdAt: String
-    
   }
+
+  # Above, defines our Schema
 
   type Query {
     users: [User]
     highScores: [HighScore]
-    
+    user(id: ID!): User
   }
 
+  # Above, defines our queries
 
-
+  type Mutation {
+    # Set the required fields for new schools
+    addUser(username: String!, email: String!, password: String!): User
+    addHighScore(highScoreTotal: String!, highScoreName: String!): HighScore
+    deleteUser(username: String!): String
+  }
+  # Above, defines which mutations the client is allowed to make
 `;
-
 
 // Above are our typedefs.
 // Note : type def and resolvers must be made before able to view in graphql
